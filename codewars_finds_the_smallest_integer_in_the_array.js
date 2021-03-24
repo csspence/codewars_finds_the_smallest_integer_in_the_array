a526ee0e1abbac5394a980271faa9e8bbb4b30c7
Crew Spence <csspence123@gmail.com>
2021-03-24 06:26:29 -0500

    Wrote working solution.

--- codewars_finds_the_smallest_integer_in_the_array.js
+++ codewars_finds_the_smallest_integer_in_the_array.js
@@ -10,6 +10,16 @@ You can assume, for the purpose of this kata, that the supplied array will not b
 
 class SmallestIntegerFinder {
   findSmallestInt(args) {
-    
+    let smallest;
+    for(let i = 0; i < args.length; i++) {
+      if(i === 0) {
+        smallest = args[i];
+      } else {
+        if(args[i] < smallest) {
+          smallest = args[i];
+        }
+      }
+    }
+    return smallest;
   }
 }
